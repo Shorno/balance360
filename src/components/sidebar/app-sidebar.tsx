9074a20af5095b051d2c3518bda569d59a112059
Shorno <roy15-6141@s.diu.edu.bd>
2025-01-17 01:03:56 +0600

    sidebar elements

---
+++ src/components/sidebar/app-sidebar.tsx
@@ -0,0 +1,168 @@
+import {
+    BookIcon,
+    GalleryVerticalEnd,
+    LayoutDashboardIcon,
+    ListOrderedIcon,
+    LogsIcon,
+    LucideListOrdered,
+    Settings2,
+    UsersIcon,
+} from "lucide-react"
+
+import {NavMain} from "@/components/sidebar/nav-main.tsx"
+import {NavUser} from "@/components/sidebar/nav-user.tsx"
+import {TeamSwitcher} from "@/components/sidebar/team-switcher.tsx"
+import {Sidebar, SidebarContent, SidebarFooter, SidebarHeader, SidebarRail,} from "@/components/ui/sidebar.tsx"
+import {NavSingle} from "@/components/sidebar/nav-single.tsx";
+import useAuthStore from "@/store/authStore.ts";
+import {useUserRole} from "@/hooks/useUserRole.tsx";
+import {Role} from "@/types";
+
+const adminNav = {
+    team: {
+        name: "X Book Store",
+        logo: GalleryVerticalEnd,
+        plan: "Enterprise",
+    },
+    navSingle: [
+        {
+            name: "Dashboard",
+            url: "",
+            icon: LayoutDashboardIcon,
+        },
+        {
+            name: "Users",
+            url: "/admin/users",
+            icon: UsersIcon,
+        },
+        {
+            name: "Books",
+            url: "/admin/books",
+            icon: BookIcon
+        },
+        {
+            name: "Orders",
+            url: "/admin/orders",
+            icon: LucideListOrdered
+        },
+    ],
+    navWithSub: [
+        {
+            title: "Management",
+            url: "#",
+            icon: LogsIcon,
+            isActive: true,
+            items: [
+                {
+                    title: "Add Book",
+                    url: "/admin/add-book",
+                },
+                {
+                    title: "Applications",
+                    url: "admin/applications",
+                },
+                {
+                    title: "Statistics",
+                    url: "admin/statistics",
+                },
+            ],
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: Settings2,
+            items: [
+                {
+                    title: "General",
+                    url: "#",
+                },
+
+                {
+                    title: "Billing",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+}
+
+const trainerNav = {
+    team: {
+        name: "X Book Store",
+        logo: GalleryVerticalEnd,
+        plan: "Enterprise",
+    },
+    navSingle: [
+        {
+            name: "Dashboard",
+            url: "",
+            icon: LayoutDashboardIcon,
+        },
+        {
+            name: "Books",
+            url: "/admin/books",
+            icon: BookIcon
+        },
+        {
+            name: "Orders",
+            url: "/admin/orders",
+            icon: ListOrderedIcon
+        },
+
+    ],
+    navWithSub: [
+        {
+            title: "Management",
+            url: "#",
+            icon: LogsIcon,
+            isActive: true,
+            items: [
+                {
+                    title: "Statistics",
+                    url: "admin/statistics",
+                },
+            ],
+        },
+        {
+            title: "Settings",
+            url: "#",
+            icon: Settings2,
+            items: [
+                {
+                    title: "General",
+                    url: "#",
+                },
+
+                {
+                    title: "Billing",
+                    url: "#",
+                },
+            ],
+        },
+    ],
+}
+
+
+export function AppSidebar({...props}: React.ComponentProps<typeof Sidebar>) {
+    const {currentUser} = useAuthStore()
+    const role: Role = useUserRole()
+    const isAdmin = role === Role.Admin
+    const links = (isAdmin ? adminNav : trainerNav)
+
+
+    return (
+        <Sidebar collapsible="icon" {...props}>
+            <SidebarHeader>
+                <TeamSwitcher team={links.team}/>
+            </SidebarHeader>
+            <SidebarContent>
+                <NavSingle navSingle={links.navSingle}/>
+                <NavMain items={links.navWithSub}/>
+            </SidebarContent>
+            <SidebarFooter>
+                <NavUser currentUser={currentUser!}/>
+            </SidebarFooter>
+            <SidebarRail/>
+        </Sidebar>
+    )
+}
---
+++ src/components/sidebar/nav-user.tsx
@@ -0,0 +1,92 @@
+import {
+    BadgeCheck,
+    Bell,
+    ChevronsUpDown,
+    CreditCard,
+    LogOut,
+    UserIcon,
+} from "lucide-react"
+
+import {
+    Avatar,
+    AvatarFallback,
+    AvatarImage,
+} from "@/components/ui/avatar.tsx"
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuGroup,
+    DropdownMenuItem,
+    DropdownMenuSeparator,
+    DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu.tsx"
+import {
+    SidebarMenu,
+    SidebarMenuButton,
+    SidebarMenuItem,
+    useSidebar,
+} from "@/components/ui/sidebar.tsx"
+import {User} from "firebase/auth";
+
+export function NavUser({currentUser}: { currentUser: User }) {
+    const {isMobile} = useSidebar()
+
+    return (
+        <SidebarMenu>
+            <SidebarMenuItem>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <SidebarMenuButton
+                            size="lg"
+                            className="data-[state=open]:bg-sidebar-accent data-[state=open]:text-sidebar-accent-foreground"
+                        >
+                            <Avatar className="h-8 w-8 rounded-lg">
+                                <AvatarImage src={currentUser?.photoURL || ""} alt={currentUser?.displayName || ""}/>
+                                <AvatarFallback className="rounded-lg">
+                                    <UserIcon/>
+                                </AvatarFallback>
+                            </Avatar>
+                            <div className="grid flex-1 text-left text-sm leading-tight">
+                                <span className="truncate font-semibold">{currentUser?.displayName || ""}</span>
+                                <span className="truncate text-xs">{currentUser?.email}</span>
+                            </div>
+                            <ChevronsUpDown className="ml-auto size-4"/>
+                        </SidebarMenuButton>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent
+                        className="w-[--radix-dropdown-menu-trigger-width] min-w-56 rounded-lg"
+                        side={isMobile ? "bottom" : "right"}
+                        align="end"
+                        sideOffset={4}
+                    >
+                        <DropdownMenuGroup>
+                                <div className={"flex items-center gap-1 text-sm"}>
+                                    Switch Theme
+                                </div>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuGroup>
+                            <DropdownMenuItem>
+                                <BadgeCheck/>
+                                Account
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <CreditCard/>
+                                Billing
+                            </DropdownMenuItem>
+                            <DropdownMenuItem>
+                                <Bell/>
+                                Notifications
+                            </DropdownMenuItem>
+                        </DropdownMenuGroup>
+                        <DropdownMenuSeparator/>
+                        <DropdownMenuItem>
+                            <LogOut/>
+                            Log out
+                        </DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </SidebarMenuItem>
+        </SidebarMenu>
+    )
+}
