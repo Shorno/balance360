595b162e0e64243c6e4a0cc3384dbd8ee4126400
Shorno <roy15-6141@s.diu.edu.bd>
2025-01-17 00:40:43 +0600

    update prod base url

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,12 @@
 import Hero from "@/components/home/Hero.tsx";
 import FeaturesSection from "@/components/FeatureSection.tsx";
+import {useUserRole} from "@/hooks/useUserRole.ts";
+import {Role} from "@/types";
 
 
 export default function Home() {
+    const role: Role = useUserRole()
+    console.log(role)
     return (
         <>
             <Hero/>
